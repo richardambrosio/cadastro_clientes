58ed0d47e13c11e08807951d0f4c0e23eb4d806f
Richard Ambrosio <richard.ambrosio1@live.com>
2023-01-11 10:04:25 -0300

    Ignorando pasta de cache das views

--- views-cache/alert.d2013cdc7d6cf427ffa7e35f5fd878e9.rtpl.php
+++
@@ -1,4 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="alert alert-<?php echo htmlspecialchars( $color, ENT_COMPAT, 'UTF-8', FALSE ); ?>" role="alert">
-    <?php echo htmlspecialchars( $text, ENT_COMPAT, 'UTF-8', FALSE ); ?>
-
-</div>
